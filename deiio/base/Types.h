1246cd9a4e14ba17fbffdcb3a48e55ec572f9466
mn <mn@furzoom.com>
2018-10-30 15:18:37 +0800

    new trunk for deiio
    Signed-off-by: mn <mn@furzoom.com>

---
+++ deiio/base/Types.h
@@ -0,0 +1,9 @@
+#ifndef DEIIO_BASE_TYPES_H_
+#define DEIIO_BASE_TYPES_H_
+
+#include <ext/vstring.h>
+#include <stdint.h>
+
+typedef __gnu_cxx::__sso_string string;
+
+#endif  // DEIIO_BASE_TYPES_H_
