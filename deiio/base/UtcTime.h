a5965452aef8d11bc007340747d7ebe5e6fe677a
yanma <yanma@mobvoi.com>
2018-11-21 17:01:04 +0800

    remove trailing whitespace

--- deiio/base/UtcTime.h
+++ deiio/base/UtcTime.h
@@ -1,3 +1,6 @@
+// Copyright 2018 furzoom.com. All Rights Reserved.
+// Author: mn@furzoom.com
+
 #ifndef DEIIO_BASE_UTCTIME_H_
 #define DEIIO_BASE_UTCTIME_H_
 
@@ -6,16 +9,16 @@
 namespace deiio {
 
 /**
- * Time stamp in UTC.
+ * Time stamp in UTC, in microseconds resolution.
  *
  * This class is immutable.
- * It's recommended to pass it by value, since it's passed in register 
+ * It's recommended to pass it by value, since it's passed in register
  * on x86_64.
  */
 class UtcTime {
  public:
   // Constructs an invalid UtcTime.
-  UtcTime(); 
+  UtcTime();
 
   /**
    * Constructs a UtcTime at specific time.
@@ -47,7 +50,10 @@ class UtcTime {
     return microSecondsSinceEpoch_;
   }
 
+  // Get time of now
   static UtcTime now();
+  static UtcTime invalid();
+
   static const int kMicroSecondsPerSecond = 1000 * 1000;
 
  private:
@@ -73,6 +79,19 @@ inline double timeDifference(UtcTime high, UtcTime low) {
   return static_cast<double>(diff) / UtcTime::kMicroSecondsPerSecond;
 }
 
+/**
+ * Add @c seconds to given timestamp.
+ *
+ * @param timestamp, seconds
+ * @return UtcTime
+ */
+inline UtcTime addTime(UtcTime timestamp, double seconds) {
+  int64_t delta =
+    static_cast<int64_t>(seconds * UtcTime::kMicroSecondsPerSecond);
+  return UtcTime(timestamp.microSecondsSinceEpoch() + delta);
+}
+
 }  // namespace deiio
 
 #endif  // DEIIO_BASE_UTCTIME_H_
+
